4d9cd100d95a49ff7487a888c2744be8250dc3be
Balazs Czoma <balazs.czoma@solace.com>
2023-06-29 19:29:39 -0400

    Updated with an alternative way to get subjectAltName

--- src/generate-server.sh
+++ src/generate-server.sh
@@ -9,9 +9,19 @@ openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.csr \
 
 echo === Sign the server cert
 cat > sign.ext << EOF
-subjectAltName = @alt_names
-[alt_names]
-DNS.1 = $SERVER_HOST
+[ req ]
+prompt = no
+distinguished_name = req_distinguished_name
+x509_extensions = san_self_signed
+[ req_distinguished_name ]
+CN=$SERVER_HOST
+[ san_self_signed ]
+subjectAltName = DNS:$SERVER_HOST, DNS:localhost, IP:127.0.0.1, IP:$SERVER_HOST
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always,issuer
+basicConstraints = CA:true
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment, dataEncipherment, keyCertSign, cRLSign
+extendedKeyUsage = serverAuth, clientAuth, timeStamping
 EOF
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -extfile sign.ext
 
